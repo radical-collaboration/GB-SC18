4120de84206ff218ff652a874c722a4dd93f3951
Jumana Dakka <jumanadakka@gmail.com>
2017-11-27 22:48:43 -0500

    ties issue with staging

--- scripts/ties/ties_barrier_gpuStack.py
+++ scripts/ties/ties_barrier_gpuStack.py
@@ -9,21 +9,30 @@ if os.environ.get('RADICAL_ENTK_VERBOSE') == None:
 
 
 class NamdTask(Task):
-    def __init__(self, name, cores):
+    def __init__(self, name):
         super(NamdTask, self).__init__()
         self.name = name
-        self.executable = ['/bin/sleep']
-        self.arguments = ['0']
-	#self.cores = cores
+        self.executable = ['/u/sciteam/jphillip/NAMD_LATEST_CRAY-XE-ugni-smp-BlueWaters/namd2']
+        #self.cores = cores
+        self.pre_exec = ['export OMP_NUM_THREADS=1']
+        self.cpu_reqs = {'processes': 1, 'process_type': 'MPI', 'threads_per_process': 31, 'thread_type': None}
         #self.mpi = mpi
-	self.cpu_reqs = {'processes': cores, 'process_type': None, 'threads_per_process': 1, 'thread_type': None}
+
 
 if __name__ == '__main__':
     # Set up parameters
 
-    cores_per_pipeline = 8
     rootdir = 'bace1_b01'
+    my_list = []
+        
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            #print os.path.join(subdir, file)
+            my_list.append(os.path.join(subdir, file))
+
+    cores_per_pipeline = 32
     pipelines = set()
+    stage_ref = []
     replicas = 5
     lambdas  = [0.0, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 1.0]
     workflow = ['min', 'eq1', 'eq2', 'prod']
@@ -34,11 +43,32 @@ if __name__ == '__main__':
     for replica in range(replicas):
         for ld in lambdas:
             p = Pipeline()
-
             for step in workflow:
-                s, t = Stage(), NamdTask(name=step, cores=cores_per_pipeline)
-               # t.arguments = ['replica_{}/lambda_{}/{}.conf'.format(replica, ld, step), '&>', 'replica_{}/lambda_{}/{}.log'.format(replica, ld, step)]
-		s.add_tasks(t)
+
+                task_ref = []
+                #s, t = Stage(), NamdTask(name=step, cores=cores_per_pipeline)
+                s, t = Stage(), NamdTask(name=step)
+                t.arguments = ['+ppn','30','+pemap', '0-29', '+commap', '30','replica_{}/lambda_{}/{}.conf'.format(replica, ld, step), '&>', 'replica_{}/lambda_{}/{}.log'.format(replica, ld, step)]
+                task_ref.append("$Pipeline_{0}_Stage_{1}_Task_{2}/".format(p.uid, s.uid, t.uid))
+                #print task_ref
+                s.add_tasks(t)
+                for task_paths in stage_ref:
+                    for task_path in task_paths:
+                        print task_path 
+                        #print count 
+                        #print workflow[stage_ref.index(task_paths)]
+                        t.copy_input_data.append(task_path+'/replica_{}/lambda_{}/{}.coor'.format(replica,ld,workflow[stage_ref.index(task_paths)]))
+                        t.copy_input_data.append(task_path+'/replica_{}/lambda_{}/{}.xsc'.format(replica,ld,workflow[stage_ref.index(task_paths)]))
+                        t.copy_input_data.append(task_path+'/replica_{}/lambda_{}/{}.vel'.format(replica,ld,workflow[stage_ref.index(task_paths)]))
+                
+                for f in my_list:
+                    t.copy_input_data.append("{}/".format(replica)+f+" > "+f)
+            
+
+            	stage_ref.append(task_ref)
+                print stage_ref
+                print count
+
                 p.add_stages(s)
 
             pipelines.add(p)
@@ -49,12 +79,10 @@ if __name__ == '__main__':
     res_dict = {
         'resource': 'ncsa.bw_aprun',
         'walltime': 1440,
-        #'cores': replicas * len(lambdas) * cores_per_pipeline,
+        'cores': replicas * len(lambdas) * cores_per_pipeline,
         'project': 'bamm',
         'queue': 'high',
-        'cpus' : replicas * len(lambdas) * cores_per_pipeline,
-	'project': 'bamm',
-	'access_schema': 'gsissh'}
+        'access_schema': 'gsissh'}
 
     # Create Resource Manager object with the above resource description
     rman = ResourceManager(res_dict)
@@ -74,4 +102,4 @@ if __name__ == '__main__':
     appman.assign_workflow(pipelines)
 
     # Run the Application Manager
-    appman.run()
+    #appman.run()
